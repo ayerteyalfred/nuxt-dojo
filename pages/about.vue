03b71c2f4528e0eabeac60075518e8ef8b4e218b
Alfred Ayertey <Alfred@maximnyansa.org>
2023-07-12 01:28:01 +0000

    First commit

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio exercitationem saepe, sequi alias id quisquam et perferendis ipsum, ipsa laborum possimus. Voluptatem repudiandae aperiam vel voluptatum quae ex rem numquam!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo vel exercitationem tempore beatae magni dolore. Vel quisquam, fugit non incidunt est a aspernatur ad ex, pariatur porro error corporis illo?</p>
+        <div>{{ data }}</div>
+    </div>
+</template>
+
+<script setup>
+    const { data } = await useFetch('/api/ninja?name=mac', {
+        method: 'post',
+        body: { age: 23 }
+    })
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+
+    p {
+        margin: 20px 0;
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio exercitationem saepe, sequi alias id quisquam et perferendis ipsum, ipsa laborum possimus. Voluptatem repudiandae aperiam vel voluptatum quae ex rem numquam!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo vel exercitationem tempore beatae magni dolore. Vel quisquam, fugit non incidunt est a aspernatur ad ex, pariatur porro error corporis illo?</p>
+    </div>
+
+</template>
+
+<script setup>
+    
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+
+    p {
+        margin: 20px 0;
+    }
+</style>
